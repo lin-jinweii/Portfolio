bc9a2d86700045583cb5f91493a7970d7ec9b845
linjinweii <linjinwei2802@gmail.com>
2022-03-26 10:21:28 +0800

    feat(): Updates on ePortfolio

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const scaleFactor = 1 / 20
 const scaleFactorMobile = 1 / 10;
 let y = window.scrollY * scaleFactor;
 
+function openMenu() {
+    document.body.classList += ' menu--open'
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
@@ -29,4 +36,52 @@ function toggleContrastMenu() {
 
 function openMenu() {
     document.body.classList += ' menu--open'
+
 }
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
+
+function toggleModalMenu() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+    document.body.classList.remove('menu--open')
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal_overlay--loading");
+    const success = document.querySelector(".modal_overlay--success");
+    loading.classList += " modal_overlay--visible";
+    emailjs
+      .sendForm(
+        "service_7by9q6g",
+        "template_7m8ykp9",
+        event.target,
+        "user_f2vttY9rqtPGHh17rdjwf"
+      )
+      .then(() => {
+        loading.classList.remove("modal_overlay--visible");
+        success.classList += " modal_overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal_overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on linjinwei2802@gmail.com"
+        );
+      });
+  }
